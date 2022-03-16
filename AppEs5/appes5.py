07c2b800d15289b8bccd32d018f453f4e537fd92
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2022-03-16 13:07:06 +0000

    .

--- AppEs5/appes5.py
+++ AppEs5/appes5.py
@@ -4,31 +4,78 @@
 
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 app = Flask(__name__)
-lst = []
+import pandas as pd
 
 @app.route('/', methods=['GET'])
-def form():
-        
-    return render_template('form.html')
+def home():
+    return render_template('squadraHome.html')
 
+@app.route('/inserisci', methods=['GET'])
+def inserisci():
+    return render_template('inserisci.html')
 
-
-@app.route('/dates', methods=['GET'])
-def es():
-
-    Nome = request.args['Nome']
-    Anno = request.args['Anno']
-    Citta = request.args['Citta']
-
-    lst.append({'Nome':Nome,'Anno':Anno,'Citta':Citta})
+@app.route('/dati', methods=['GET'])
+def dati():
+    # inserimento dei dati nel file csv
+    # lettura dei dati dal form html 
+    squadra = request.args['Squadra']
+    anno = request.args['Anno']
+    citta = request.args['Citta']
+    # lettura dei dati daal file nel dataframe
+    df1 = pd.read_csv('/workspace/Flask/AppEs5/dati.csv')
+    # aggiungiamo i nuovi dati nel dataframe 
+    nuovi_dati = {'squadra':squadra,'anno':anno,'citta':citta}
     
+    df1 = df1.append(nuovi_dati,ignore_index=True)
+    # salviamo il dataframe sul file dati.csv
+    df1.to_csv('/workspace/Flask/AppEs5/dati.csv', index=False)
+    return df1.to_html()   #lo converte in codice html
+
+
+@app.route('/ricerca', methods=['GET'])
+def ricerca():
+    return render_template('ricerca.html')
+
+
+
+@app.route('/ric', methods=['GET'])
+def ric():
+
+df2 = pd.read_csv('/workspace/Flask/AppEs5/dati.csv')
+gia_inseriti = {'squadra':squadra,'anno':anno,'citta':citta}
+
+    squadra = request.args['Squadra']
+    anno = request.args['Anno']
+    citta = request.args['Citta']
+
+    for elemento in nuovi_dati:
+        if elemento['squadra'] == squadra:
+            if utente['sex'] == 'M':
+                return render_template('welcome.html', nome=utente['name'])
+            else:
+                return render_template('welcomeW.html', nome=utente['name'])
+
+    return render_template('error.html')
+
     
-    return 
 
 
-
+    # inserimento dei dati nel file csv
+    # lettura dei dati dal form html 
+    #squadra = request.args['Squadra']
+    #anno = request.args['Anno']
+    #citta = request.args['Citta']
+    # lettura dei dati daal file nel dataframe
+    #df1 = pd.read_csv('/workspace/Flask/AppEs5/dati.csv')
+    # aggiungiamo i nuovi dati nel dataframe 
+    #nuovi_dati = {'squadra':squadra,'anno':anno,'citta':citta}
+    
+    #df1 = df1.append(nuovi_dati,ignore_index=True)
+    # salviamo il dataframe sul file dati.csv
+    #df1.to_csv('/workspace/Flask/AppEs5/dati.csv', index=False)
+    #return df1.to_html()   #lo converte in codice html
 
 
 
@@ -36,4 +83,11 @@ def es():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3246, debug=True)
+
+
+
+
+
+
+
